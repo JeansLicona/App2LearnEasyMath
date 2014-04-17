fad74fccf07eb3029e2a18a019dd65e0f54d3b39
Wilberth Rivas <cloud_ultimate@hotmail.com>
2014-04-17 18:00:26 -0500

    Primera revision

--- chat/save_message.php
+++ chat/save_message.php
@@ -1,35 +1,32 @@
 <?php
 
-$message = $_POST['TxtMessage'];
+if (isset($_POST['TxtMessage'])) {
+    $message = $_POST['TxtMessage'];
 //$date = date("Y-m-d");
-$date = date_create()->format('Y-m-d H:i:s');
+    $date = date_create()->format('Y-m-d H:i:s');
 
 //var_dump($_POST);
 //die();
-include("lib.php");
+    include("lib.php");
 
-     $sSQL = "INSERT INTO mensaje(usuario_id,mensaje,fecha)values('1','$message','$date')";
-        
-		$content_all = "";
-		
-		$date_array = explode(" ",$date);
-		$current_date_array = explode("-",$date_array[0]);
-		
-		$current_date = $current_date_array[2]."/". $current_date_array[1] . "/" .$current_date_array[0];
-		
-    if($oResult = mysql_query($sSQL)) {
-        //$sStatus = "Added successful";
-		$content_all = array("message"=>"<div> Fco: ".$message."<br />".$date_array[1]." ". $current_date ."</div>");
-
-		
-     } else {
-        //$sStatus = "An error occurred while inserting; customer not saved.";
-		$content_all = array("message"=>"<div>".$message." " .$date." (mensaje no enviado)</div>");
-    }
+    $sSQL = "INSERT INTO mensaje(usuario_id,mensaje,fecha)values('1','$message','$date')";
     
+    $content_all = "";
+    $date_array = explode(" ", $date);
+    $current_date_array = explode("-", $date_array[0]);
+
+    $current_date = $current_date_array[2] . "/" . $current_date_array[1] . "/" . $current_date_array[0];
+
+    if ($oResult = mysql_query($sSQL)) {
+        //$sStatus = "Added successful";
+        $content_all = array("message" => "<div> Fco: " . $message . "<br />" . $date_array[1] . " " . $current_date . "</div>");
+    } else {
+        //$sStatus = "An error occurred while inserting; customer not saved.";
+        $content_all = array("message" => "<div>" . $message . " " . $date . " (mensaje no enviado)</div>");
+    }
+
     //mysql_free_result($oResult);
     mysql_close($oLink);
-	
-	echo json_encode($content_all);
-	
+    echo json_encode($content_all);
+}
 ?>
