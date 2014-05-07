d61eebf74371803d7d917b276fbee101508eaaac
VenturaPerez <ventura.perez1991@hotmail.com>
2014-05-06 19:14:34 -0500

    mis cambios

--- Aplicacion/sitio/index.php
+++ Aplicacion/sitio/index.php
@@ -1,14 +1,15 @@
-<?php
-    /*
-     * To change this template, choose Tools | Templates
-     * and open the template in the editor.
-     */
+﻿<?php
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
 ?>
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 
         <link href="../jquery/css/ui-darkness/jquery-ui-1.10.4.custom.css" rel="stylesheet">
+		<link href="../estilos/style.css" rel="stylesheet">
         <script src="../jquery/js/jquery-1.10.2.js"></script>
         <script src="../jquery/js/jquery-ui-1.10.4.custom.js"></script>
         <script type="text/javascript" src="../extensiones/DataTables-1.8.0/media/js/jquery.dataTables.js"></script>
@@ -34,20 +35,6 @@
             }
             function render(url) {
                 $.get(url, function(data) {
-                    if ($("#dialog-create").length) {
-                        var isOpen = $("#dialog-create").dialog("isOpen");
-                        if (isOpen) {
-                            $("#dialog-create").dialog("close");
-                        }
-                        $("#dialog-create").dialog("destroy");
-                    }
-                    if ($("#dialog-update").length) {
-                        var isOpen = $("#dialog-update").dialog("isOpen");
-                        if (isOpen) {
-                            $("#dialog-update").dialog("close");
-                        }
-                        $("#dialog-update").dialog("destroy");
-                    }
                     $("#contenido").html(data);
                     if ($("#dialog-create").length && $("#dialog-create-link").length) {
                         $("#dialog-create").dialog({
@@ -100,13 +87,6 @@
                             event.preventDefault();
                             $.get($(this).attr('href'),
                                     function(data) {
-                                        if ($("#dialog-update").length) {
-                                            var isOpen = $("#dialog-update").dialog("isOpen");
-                                            if (isOpen) {
-                                                $("#dialog-update").dialog("close");
-                                            }
-                                            $("#dialog-update").dialog("destroy");
-                                        }
                                         $("#update-contenedor").html(data);
                                         $("#dialog-update").dialog({
                                             autoOpen: true,
@@ -139,55 +119,13 @@
             }
         </script>
         <style type="text/css">
-            div#contenido { 
-                clear:both; 
-            }
-            body{
-                font: 82.5% "Trebuchet MS", sans-serif;
-                margin: 50px;
-            }
-
-            #dialog-link {
-                padding: .4em 1em .4em 20px;
-                text-decoration: none;
-                position: relative;
-            }
-            #dialog-link span.ui-icon {
-                margin: 0 5px 0 0;
-                position: absolute;
-                left: .2em;
-                top: 50%;
-                margin-top: -8px;
-            }
-            #icons {
-                margin: 0;
-                padding: 0;
-            }
-            #icons li {
-                margin: 2px;
-                position: relative;
-                padding: 4px 0;
-                cursor: pointer;
-                float: left;
-                list-style: none;
-            }
-            #icons span.ui-icon {
-                float: left;
-                margin: 0 4px;
-            }
-            .fakewindowcontain .ui-widget-overlay {
-                position: absolute;
-            }
-            #error{
-                color: red;
-                background-color: #F08080;
-            }
+           
         </style>
     </head>
     <body>
         <div id="menu-principal">
             <ul class="menu">
-                <li><a href="#">Inicio</a></li>
+                <li class = "active"><a href="#">Inicio</a></li> <a style = "font-size: 0px;"> | </a>
                 <li><a href="#">Administracion</a>
                     <ul>
                         <li><a href="#" onclick="render('../alumno/admin.php');">Alumnos</a>
@@ -195,16 +133,16 @@
                         <li><a href="#" onclick="render('../tutor/admin.php');">Tutores</a></li>
                     </ul>
                 </li>
-                <li><a href="#" onclick="render('../plan/admin.php');">Plan</a></li>
+				<li><a href="#" onclick="render('../plan/admin.php');">Plan</a></li>
                 <li><a href="#" onclick="render('../tarea/admin.php');">Tarea</a></li>
-                <li><a href="#" onclick="render('../grupo/admin.php');">Grupo</a></li>
-                <li><a href="#">Contacto</a></li>
-                <li><a href="#">Acerca de</a></li>
+                <li><a href="#" onclick="render('../grupo/admin.php');">Grupo</a></li>	
+                <li><a href="#">Contacto</a></li>  
+                <li><a href="#">Acerca de</a></li> 
                 <li><a href="#">Logout</a></li>
-                <li><a href="../../chat/index.php">Chat</a></li>
             </ul>
         </div>
-        <div id="contenido">
+        
+		<div id="contenido">
 
         </div>
     </body>
