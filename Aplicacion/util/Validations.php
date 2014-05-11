64e4c08ff6dfd49ce856de1bcead6c11cc884fa2
Wilberth Rivas <cloud_ultimate@hotmail.com>
2014-05-11 18:18:24 -0500

    Archivo que implementa las funciones de validaciones

---
+++ Aplicacion/util/Validations.php
@@ -0,0 +1,38 @@
+<?php
+
+function isEmpty($value) {
+    if (isset($value)) {
+        if (trim($value) != '') {
+            return false;
+        }
+    }
+    return true;
+}
+
+function isEmail($email) {
+    if (filter_var($email, FILTER_VALIDATE_EMAIL) !== false) {
+        return true;
+    }
+    return false;
+}
+
+function isAlfaNumeric($value) {
+    if (preg_match('/^[1-9A-Za-z]*$/', $value)) {
+        return true;
+    }
+    return false;
+}
+function isNumeric($value) {
+    if (preg_match('/^[1-9]*$/', $value)) {
+        return true;
+    }
+    return false;
+}
+
+function isDateFormat($value) {
+    if (preg_match('/^\d{2}\/\d{2}\/\d{2}*$/', $value)) {
+        return true;
+    }
+    return false;
+}
+?>
