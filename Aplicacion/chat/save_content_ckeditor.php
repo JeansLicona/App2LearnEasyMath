5c37e888aee8e7f1322fbd9d29b7aceda172f18b
Wilberth Rivas <cloud_ultimate@hotmail.com>
2014-05-23 14:20:04 -0500

    Actualizacion del CKEditor

---
+++ Aplicacion/chat/save_content_ckeditor.php
@@ -0,0 +1,41 @@
+<?php
+
+session_start();
+
+if(!isset($_SESSION['logueado']) && $_SESSION['logueado']!='si')
+{
+	header('Location: ../sitio/login.php');
+	exit();
+}
+
+if(isset($_POST['rand']) && trim($_POST['rand'])!='' && 
+isset($_POST['id']) && !empty($_POST['id']) && trim($_POST['id'])!='' && 
+isset($_POST['content_ckeditor']) )
+{
+	include("../validacion/lib.php");
+
+	$link = connectDB();
+
+	$message = htmlspecialchars(mysqli_real_escape_string($link,$_POST['content_ckeditor']));
+
+	$content = $_POST['content_ckeditor'];
+	//$id_tarea = $_POST['id'];
+	$id_tarea = 1;
+	
+	$sql_update = "UPDATE tarea SET contenido='$content' WHERE id_tarea='$id_tarea'";
+	
+    if($result = mysqli_query($link,$sql_update)) 
+	{
+		//$all_content = array("message"=>"<div> Fco: ".$message."<br />".$date_array[1]." ". $current_date ."</div>");
+
+     } else {
+        
+		$all_content = array("message"=>"<div>(mensaje no enviado)</div>");
+    }
+    
+    //mysqli_free_result($result);
+	mysqli_close($link);
+	
+	echo json_encode($all_content);
+}
+?>
